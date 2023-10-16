15f646643f5bf95b2a0891c6ce894f859aa48519
jjdubski <jjwaksmanski@gmail.com>
2023-10-16 04:43:12 -0500

    more features

---
+++ dropdown.js
@@ -0,0 +1,6 @@
+const classSelect = document.getElementById("classSelect");
+classSelect.addEventListener("change", function() {
+    const selectedClass = classSelect.value;
+    // Do something with the selected class
+    console.log(selectedClass);
+});
